f688efa5efab7026456273db655a5bf1b12a4e45
Raulitoflying <1017329098@qq.com>
2024-09-12 11:21:16 -0700

    Add Input component

---
+++ components/Input.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, View, Text, StatusBar } from 'react-native'; 
+
+const Input = () => {
+  const [inputText, setInputText] = useState('');
+
+  return (
+    <View>
+      <TextInput
+        style={styles.input}
+        placeholder="Type here..."
+        value={inputText}
+        onChangeText={setInputText}
+      />
+      <Text>You typed: {inputText}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    marginVertical: 20,
+    width: '80%',
+  },
+});
+
+export default Input;
