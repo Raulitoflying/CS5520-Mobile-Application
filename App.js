206bbf998d54a38a8b1812967424802974a8fc4f
Raulitoflying <1017329098@qq.com>
2024-09-19 11:16:11 -0700

    Refactor App component to include autoFocus prop and handle input data

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
 import Input from './components/Input';
 
 export default function App() {
+  const [inputData, setInputData] = useState('');
   const appName = "CS5520-Mobile-Application";
+
+  const handleInputData = (data) => {
+    setInputData(data);
+  };
 
   return (
     <View style={styles.container}>
+      <Text>Welcome to {appName}</Text>
       <Header appName={appName} />
-      <Input />
+      <Input autoFocus={true} onConfirm={handleInputData} />
+      <Text>You typed: {inputData}</Text>
       <StatusBar style="auto" />
     </View>
   );
