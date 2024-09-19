b3e30c56f9618af9ee280c381f3837eb1af97c9a
Raulitoflying <1017329098@qq.com>
2024-09-19 11:29:03 -0700

    Refactor App component to include Button component for adding a goal and close the modal after input confirmation

--- App.js
+++ App.js
@@ -1,22 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import Header from './components/Header';
 import Input from './components/Input';
 
 export default function App() {
   const [inputData, setInputData] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
   const appName = "CS5520-Mobile-Application";
 
   const handleInputData = (data) => {
     setInputData(data);
+    setModalVisible(false); // Close the modal
   };
 
   return (
     <View style={styles.container}>
       <Text>Welcome to {appName}</Text>
       <Header appName={appName} />
-      <Input autoFocus={true} onConfirm={handleInputData} />
+      <Button title="Add a goal" onPress={() => setModalVisible(true)} />
+      <Input autoFocus={true} onConfirm={handleInputData} visible={modalVisible} />
       <Text>You typed: {inputData}</Text>
       <StatusBar style="auto" />
     </View>
