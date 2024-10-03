6b634aa0ecd1dbb2636812c632a08d56cf001cb6
Raulitoflying <1017329098@qq.com>
2024-10-03 11:15:04 -0700

    Import createNativeStackNavigator in App.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'; // 导入 createNativeStackNavigator
 import Home from './components/Home';
 
 export default function App() {
