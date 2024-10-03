ec6f5e9eb9e901987e2317e20757b169b37095c2
Raulitoflying <1017329098@qq.com>
2024-10-03 12:51:20 -0700

    Refactor App.js to include custom header styling for the Home screen

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './components/Home';
-import GoalDetails from './components/GoalDetails'; 
+import React from "react";
+import Home from "./components/Home";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import GoalDetails from "./components/GoalDetails";
+import { Button } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,19 +11,34 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} 
-          options={{ 
-            title: 'My Goals App',
-            headerStyle: {
-              backgroundColor: 'darkmagenta',
-            },
-            headerTintColor: 'white',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerStyle: { backgroundColor: "purple" },
+            headerTintColor: "white",
+            title: "My Goals",
           }}
         />
-        <Stack.Screen name="Details" component={GoalDetails} /> 
+        <Stack.Screen
+          name="Details"
+          component={GoalDetails}
+          options={({ route }) => {
+            return {
+              title: route.params ? route.params.goalData.text : "More Details",
+              headerRight: () => {
+                return (
+                  <Button
+                    title="Warning"
+                    onPress={() => {
+                      console.log("warning");
+                    }}
+                  />
+                );
+              },
+            };
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
