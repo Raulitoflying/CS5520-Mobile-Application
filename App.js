9f504ce7785cf2caaf9d9bc920b532b17f8e51cf
Raulitoflying <1017329098@qq.com>
2024-10-03 11:16:03 -0700

    Configure Stack.Navigator and Stack.Screen in App.js

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'; // 导入 createNativeStackNavigator
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './components/Home';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Home />
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
