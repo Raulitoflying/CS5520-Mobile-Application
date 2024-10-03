6dc7b3858db40bbc9d66cf9601a4cb86c328843e
Raulitoflying <1017329098@qq.com>
2024-10-03 16:31:31 -0700

    Refactor GoalDetails component to update goal parameter name, improve goal details display, and add warning functionality

--- components/GoalDetails.js
+++ components/GoalDetails.js
@@ -1,8 +1,30 @@
 import { Button, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function GoalDetails({ navigation, route }) {
-  console.log(route);
+  const [textColor, setTextColor] = useState("purple");
+  const [isWarning, setIsWarning] = useState(false);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Button
+          onPress={handleWarning} 
+          title="Warning"
+          color="red"
+        />
+      ),
+    });
+  }, [navigation]);
+
+  const handleWarning = () => {
+    setTextColor("red");
+    setIsWarning(true);
+    navigation.setOptions({
+      title: "Warning!",
+    });
+  };
+
   function moreDetailsHandler() {
     navigation.push("Details");
   }
@@ -10,16 +32,23 @@ export default function GoalDetails({ navigation, route }) {
   return (
     <View>
       {route.params ? (
-        <Text>
+        <Text style={[styles.text, { color: textColor }]}>
           This is details of a goal with text {route.params.goalObj.text} and
           id {route.params.goalObj.id}
         </Text>
       ) : (
-        <Text>More details</Text>
+        <Text style={[styles.text, { color: textColor }]}>
+          More details</Text>
       )}
       <Button title="More Details" onPress={moreDetailsHandler} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    margin: 10,
+    marginBottom: 10,
+  },
+});
