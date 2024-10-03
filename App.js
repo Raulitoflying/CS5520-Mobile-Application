b4b3ff1d4dcef1a47eba66bddc2bee47e1ea2fbb
Raulitoflying <1017329098@qq.com>
2024-10-03 14:54:21 -0700

    Refactor to use common header styles for all screens

--- App.js
+++ App.js
@@ -7,18 +7,19 @@ import { Button } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
+const commonHeaderOptions = {
+  headerStyle: { backgroundColor: "purple" },
+  headerTintColor: "white",
+};
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={commonHeaderOptions}>
         <Stack.Screen
           name="Home"
           component={Home}
-          options={{
-            headerStyle: { backgroundColor: "purple" },
-            headerTintColor: "white",
-            title: "My Goals",
-          }}
+          options={{ title: "My Goals" }}
         />
         <Stack.Screen
           name="Details"
