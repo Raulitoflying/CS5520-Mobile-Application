bf5eb9773def8c1e69454288ec459f217e801649
Raulitoflying <1017329098@qq.com>
2024-10-17 11:14:03 -0700

    Add firebase setup file

---
+++ firebase/firebaseSetup.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC31BU-o8x9WB7piYeqwIK4Xg5jafnoBrc",
+  authDomain: "cs5520-mobile-applicatio-dd3b1.firebaseapp.com",
+  projectId: "cs5520-mobile-applicatio-dd3b1",
+  storageBucket: "cs5520-mobile-applicatio-dd3b1.appspot.com",
+  messagingSenderId: "414341646484",
+  appId: "1:414341646484:web:8f7644b4aa19157eb60c81"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
