68ec33bfce1c3d0919a76f012432098d883319f5
Raulitoflying <1017329098@qq.com>
2024-10-31 11:39:16 -0700

    Refactor Firebase authentication setup to use React Native persistence with AsyncStorage

--- firebase/firebaseSetup.js
+++ firebase/firebaseSetup.js
@@ -1,7 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth'
+import { initializeAuth , getReactNativePersistence} from 'firebase/auth'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+// import { getAuth } from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +20,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app);
-export const auth = getAuth(app);
+// export const auth = getAuth(app);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
+
